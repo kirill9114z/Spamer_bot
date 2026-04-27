8d8ea09421147045ebe10ab7f44f36c232067661
Edgelik <113557308+kirill9114z@users.noreply.github.com>
2026-04-27 11:07:15 +0300

    Update account_manager.py

--- account_manager.py
+++ account_manager.py
@@ -4,7 +4,6 @@ from proxy_manager import ACCOUNTS
 
 
 def register_account(session_name, proxy=None):
-    """Регистрирует новый аккаунт в системе."""
     with Client(ACCOUNTS[]['session_name'], api_id=ACCOUNTS[]['api_id'], api_hash=ACCOUNTS[]['api_hash'] as app:
         app.start()
         add_account(session_name, proxy)
