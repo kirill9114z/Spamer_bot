01ee1e35a5e70c9f00ea4a18b8402ee3c5f963b1
Edgelik <113557308+kirill9114z@users.noreply.github.com>
2026-04-27 11:07:02 +0300

    Update gather_users.py

--- gather_users.py
+++ gather_users.py
@@ -2,10 +2,6 @@ from database import add_user, add_group, update_group_last_updated
 
 
 async def gather_users_from_group(group_name, client, account):
-    """
-    Получает список участников группы и добавляет их в базу данных.
-    """
-
     try:
 
         print(f"Подключение к группе {group_name} с аккаунта {account['session_name']}...")
@@ -15,7 +11,7 @@ async def gather_users_from_group(group_name, client, account):
 
         print(f"Сбор участников группы {group.title}...")
         total_members = 0
-        async for member in client.get_chat_members(group.id):  # Используем async for
+        async for member in client.get_chat_members(group.id):  
             if not member.user.is_bot and not member.user.is_deleted:
                 user_id = member.user.id
                 username = member.user.username
